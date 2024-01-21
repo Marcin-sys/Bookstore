2f03dda8f398f9ac9a8481ff7ca53eb5740e4437
MarcinMirocha <mirocha.marcin77@gmail.com>
2024-01-21 12:38:23 +0100

    Lombok for models

--- src/main/java/pl/mirocha/marcin/it/book/store/model/Book.java
+++ src/main/java/pl/mirocha/marcin/it/book/store/model/Book.java
@@ -1,73 +1,22 @@
 package pl.mirocha.marcin.it.book.store.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Book implements Cloneable {
     private int id;
     private String title;
     private String author;
     private String isbn;
     private double price;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
     private int quantity;
 
-    public Book() {
-    }
-
-    public Book(int id, String title, String author, String isbn, double price, int quantity) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.price = price;
-        this.quantity = quantity;
-    }
 
     @Override
     public Book clone() {
--- src/main/java/pl/mirocha/marcin/it/book/store/model/User.java
+++ src/main/java/pl/mirocha/marcin/it/book/store/model/User.java
@@ -1,5 +1,14 @@
 package pl.mirocha.marcin.it.book.store.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
     private int id;
     private String login;
@@ -7,66 +16,6 @@ public class User {
     private String name;
     private String surname;
     private String role;
-
-    public User() {
-    }
-
-    public User(int id, String login, String password, String name, String surname, String role) {
-        this.id = id;
-        this.login = login;
-        this.password = password;
-        this.name = name;
-        this.surname = surname;
-        this.role = role;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 
     @Override
     public User clone() {
